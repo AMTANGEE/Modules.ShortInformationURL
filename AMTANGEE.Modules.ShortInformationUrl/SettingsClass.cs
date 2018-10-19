2f9e3bbe9ad416153c3d763603f73eb875f8030b
Jörg Wächtler <waechtler@1click.de>
2018-10-19 12:58:18 +0200

    add ClassSettings and Icon

---
+++ AMTANGEE.Modules.ShortInformationUrl/SettingsClass.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AMTANGEE.Modules.ShortInformationUrl
+{
+    class SettingsClass
+    {
+        static string Settings_Name = "AMTANGEE.MODULES.SHORTINFORMATIONURL";
+
+        private static string _URL = GetSettingsString("URL");
+        public static string URL
+        {
+            get
+            {
+                if (_URL == null)
+                    _URL = "";
+                return _URL;
+            }
+            set
+            {
+                _URL = value;
+                SetSettings("URL", value);
+            }
+        }
+        private static string _TEXT = GetSettingsString("TEXT");
+        public static string TEXT
+        {
+            get
+            {
+                if (_TEXT == null)
+                    _TEXT = "";
+                return _TEXT;
+            }
+            set
+            {
+                _TEXT = value;
+                SetSettings("TEXT", value);
+            }
+        }
+        private static Image _DisplayImage = GetSettingsImage("IMAGE");
+        public static Image IMAGE
+        {
+            get
+            {
+                if (_DisplayImage == null)
+                    _DisplayImage = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Website];
+                return _DisplayImage;
+            }
+            set
+            {
+                _DisplayImage = value;
+                SetSettings("IMAGE", value);
+            }
+        }
+        private static void SetSettings(string identifier, Image value)
+        {
+            try
+            {
+                AMTANGEE.SDK.Settings.Global[Settings_Name][identifier] = ToText(value).ToString();
+            }
+            catch
+            {
+            }
+        }
+        private static void SetSettings(string identifier, string value)
+        {
+            try
+            {
+                AMTANGEE.SDK.Settings.Global[Settings_Name][identifier] = value.ToString();
+            }
+            catch
+            {
+            }
+        }
+        private static string GetSettingsString(string identifier)
+        {
+            try
+            {
+                return AMTANGEE.SDK.Settings.Global[Settings_Name][identifier];
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private static Image GetSettingsImage(string identifier)
+        {
+            try
+            {
+                return ToImage(Convert.ToString(AMTANGEE.SDK.Settings.Global[Settings_Name][identifier]));
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        private static string ToText(Image Image)
+        {
+            if (Image != null)
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    byte[] imageBytes = ms.ToArray();
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            else
+            {
+                return null;
+            }
+        }
+        private static Image ToImage(string newData)
+        {
+            Image _return = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Website];
+            try
+            {
+                if (newData.Length > 0)
+                {
+                    byte[] byteumrechner = Convert.FromBase64String(newData);
+                    if (byteumrechner != null && byteumrechner.Length > 0)
+                    {
+                        var _with2 = new System.Drawing.ImageConverter();
+                        _return = (Image)_with2.ConvertFrom(byteumrechner);
+                    }
+                }
+            }
+            catch
+            {
+                _return = null;
+            }
+            return _return;
+        }
+    }
+}
--- AMTANGEE.Modules.ShortInformationUrl/SettingsControl.cs
+++ AMTANGEE.Modules.ShortInformationUrl/SettingsControl.cs
@@ -17,15 +17,86 @@ namespace AMTANGEE.Modules.ShortInformationUrl
 
         public void LoadData()
         {
-            edtURL.Text = AMTANGEE.SDK.Settings.Global["AMTANGEE.MODULES.SHORTINFORMATIONURL"]["URL"];
-            edtText.Text = AMTANGEE.SDK.Settings.Global["AMTANGEE.MODULES.SHORTINFORMATIONURL"]["TEXT"];
+            edtURL.Text = SettingsClass.URL;
+            edtText.Text = SettingsClass.TEXT;
+            btnImage.Image = SettingsClass.IMAGE;
+            btnResetImage.Image = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Delete];
         }
-
-
         public void SaveData()
         {
-            AMTANGEE.SDK.Settings.Global["AMTANGEE.MODULES.SHORTINFORMATIONURL"]["URL"] = edtURL.Text;
-            AMTANGEE.SDK.Settings.Global["AMTANGEE.MODULES.SHORTINFORMATIONURL"]["TEXT"] = edtText.Text;
+            SettingsClass.URL = edtURL.Text;
+            SettingsClass.TEXT = edtText.Text;
+            SettingsClass.IMAGE = btnImage.Image;
+        }
+        private static string ImageToText(Image Image)
+        {
+            if (Image != null)
+                using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                {
+                    Image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                    byte[] imageBytes = ms.ToArray();
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            else
+            {
+                return null;
+            }
+        }
+        private static Image TextToImage(string newData)
+        {
+            Image _return = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Website];
+            try
+            {
+                if (newData.Length > 0)
+                {
+                    byte[] byteumrechner = Convert.FromBase64String(newData);
+                    if (byteumrechner != null && byteumrechner.Length > 0)
+                    {
+                        var _with2 = new System.Drawing.ImageConverter();
+                        _return = (Image)_with2.ConvertFrom(byteumrechner);
+                    }
+                }
+            }
+            catch
+            {
+                _return = null;
+            }
+            return _return;
+        }
+        private void btnImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string GraphicFileFilter = "Alle Bilddateien|*.jpg;*.jpeg;*.jpe;*.jfif;*.bmp;*.gif;*.png;*.ico";
+            string AllFileFilter = "Alle Dateien|*.*";
+            openFileDialog.Title = "Bild öffnen";
+            openFileDialog.Multiselect = false;
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.Filter = GraphicFileFilter + "|" + AllFileFilter;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.FileName = "";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = openFileDialog.FileName;
+                try
+                {
+                    if (btnImage.Image != null)
+                    {
+                        btnImage.Image = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Website];
+                    }
+                    btnImage.Image= Image.FromFile(fileName);
+                }
+                catch (Exception ex)
+                {
+                    string messageText = String.Format("Die Datei '{0}' konnte nicht geladen werden:{1}{2}", fileName, Environment.NewLine, ex.Message);
+                    MessageBox.Show(this, messageText, this.Text, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+        }
+        private void btnResetImage_Click(object sender, EventArgs e)
+        {
+            btnImage.Image = AMTANGEE.SDK.Resources.Images[SDK.Resources.ImageIndexes.Website];
         }
     }
 }
